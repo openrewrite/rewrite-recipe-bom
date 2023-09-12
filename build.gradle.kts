26d15907a02f60f48d9707407415791a78a6c08a
Sam Snyder <sam@moderne.io>
2023-09-12 12:08:36 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
     api("org.openrewrite.recipe:rewrite-hibernate:$latest")
     api("org.openrewrite.recipe:rewrite-java-dependencies:$latest")
     api("org.openrewrite.recipe:rewrite-java-security:$latest")
-    api("org.openrewrite.recipe:rewrite-java-dependencies:$latest")
     api("org.openrewrite.recipe:rewrite-jenkins:$latest")
     api("org.openrewrite.recipe:rewrite-jhipster:$latest")
     api("org.openrewrite.recipe:rewrite-hibernate:$latest")
