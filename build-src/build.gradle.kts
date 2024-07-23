ee3ecee4c8ed0103459e83d81a5ef286dcec8838
Tim te Beek <tim@moderne.io>
2024-07-23 10:34:54 +0200

    Adopt nebula.release plugin v19 after failure to resolve grgit (#29)

--- build-src/build.gradle.kts
+++ build-src/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     implementation("org.owasp:dependency-check-gradle:latest.release")
     implementation("com.netflix.nebula:gradle-contacts-plugin:6.0.0")
     implementation("com.netflix.nebula:gradle-info-plugin:11.3.3")
-    implementation("com.netflix.nebula:nebula-release-plugin:16.0.0")
+    implementation("com.netflix.nebula:nebula-release-plugin:19.0.10")
     implementation("com.netflix.nebula:nebula-publishing-plugin:18.4.0")
     implementation("com.netflix.nebula:nebula-project-plugin:9.6.3")
     implementation("io.github.gradle-nexus:publish-plugin:1.0.0")
