c0e850677d58a5f1a2660bfd42eac6ba12079448
Tim te Beek <tim@moderne.io>
2025-01-16 23:24:24 +0100

    Manage version of MSAL rewrite-github-actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     api("org.openrewrite.recipe:rewrite-cucumber-jvm:$latest")
     api("org.openrewrite.recipe:rewrite-docker:$latest")
     api("org.openrewrite.recipe:rewrite-feature-flags:$latest")
+    api("org.openrewrite.recipe:rewrite-github-actions:$latest")
     api("org.openrewrite.recipe:rewrite-gitlab:$latest")
     api("org.openrewrite.recipe:rewrite-hibernate:$latest")
     api("org.openrewrite.recipe:rewrite-java-dependencies:$latest")
